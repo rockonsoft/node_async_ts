353fb1427efd57a3e38ea27596e70306cd5d4ff1
Hano <hanobotha@rockonsoft.com>
2019-09-04 05:56:38 +0200

    add vscode launch and minimist for commandline args

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
+import * as minimist from 'minimist';
+const argv = minimist.default(process.argv.slice(2));
 
-
-async function main(){
-    console.log('Hello to the world!')
+async function sayName(name: string) {
+  console.log(`Hello ${name}`);
 }
 
-main();
+if (argv.name) {
+  sayName(argv.name).then(() => {
+    console.log('Utility completed.');
+  });
+}
+
+if (argv.help || !argv.name) {
+  console.log('Usage:');
+  console.log('--name, Prints the name specified.');
+  console.log('--help, Prints the usage documentation.');
+}
